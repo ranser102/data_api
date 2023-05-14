076581838fb83cae3869be4da18f401108c120f9
Eran Sery <ranser102@gmail.com>
2023-05-14 13:49:25 -0400

    Change file names

---
+++ data_api.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+import joblib
+
+# Load the model
+the_model = joblib.load("a_model.joblib")
+
+# Create the FastAPI app
+app = FastAPI()
+
+# Define the prediction endpoint
+@app.get("/predict")
+def predict(height: int, weight: int, shoe: int):
+    # Make predictions using the loaded model
+    prediction = the_model.predict([[height, weight, shoe]])
+    return {"prediction": prediction[0]}
+
+# Run the FastAPI server
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("data_api:app", host="0.0.0.0", port=8000, reload=True)
